49b3eeb9b5b90b3e89d1e424288ff7e43ddceb5b
AdrianMoraJ <amora19072004@gmail.com>
2024-03-07 22:22:34 +0100

    ViewModel and Factory

--- app/src/main/java/com/example/adrian_clementinomora_practicapmdm/ui/MainActivity.kt
+++ app/src/main/java/com/example/adrian_clementinomora_practicapmdm/ui/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.adrian_clementinomora_practicapmdm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.adrian_clementinomora_practicapmdm.R
+import com.example.adrian_clementinomora_practicapmdm.database.ArticleDatabase
+import com.example.adrian_clementinomora_practicapmdm.databinding.ActivityMainBinding
+import com.example.adrian_clementinomora_practicapmdm.repository.NewsRepository
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
     }
 }
